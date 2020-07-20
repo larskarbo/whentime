e5fa0b0f647b2c2336df73a4411fff06c6274927
Lars Karbo <mail@larskarbo.no>
2020-07-20 08:11:45 +0200

    mvp

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.scss";
+import moment from "moment";
+import "moment-timezone";
+import cityTimezones from "city-timezones";
+
+import { Grommet, Box, Button, Image, Text } from "grommet";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const string = window.location.pathname.substring(1)
+	const infos = string.split("-")
+	console.log('string: ', string);
+
+	const time = infos.find(i => {
+		if (i.includes("pm") || i.includes("am")) {
+			return i
+		}
+	})
+	console.log(time)
+	let lastIn = false
+	const city = infos.find(i => {
+		if (i == "in") {
+			lastIn = true
+			return
+		}
+		if (lastIn) {
+			return true
+		}
+	})
+
+	const cityInfo = cityTimezones.lookupViaCity(city).sort((a,b) => b.pop - a.pop).shift()
+	console.log('cityInfo: ', cityInfo);
+
+	const timeOriginal = moment(time, "ha")
+
+	const timeLocal = moment.tz(time, "ha", cityInfo.timezone).local();
+	const myTz = Intl.DateTimeFormat().resolvedOptions().timeZone
+
+
+
+	const myTheme = {
+		global: {
+			background: "white"
+		}
+	};
+	return (
+		<div className="App">
+			<div pad="large">
+				<div weight={200} size="70px">When time is <span weight={800} size="100px">{timeOriginal.format("h:mm a")}</span> in {cityInfo.city} ({cityInfo.timezone}) ...</div>
+				<div weight={200} size="70px">... it is <span weight={800} size="100px">{timeLocal.format("h:mm a")}</span> in your local time ({myTz})</div>
+			</div>
+			<div>
+				{/* <span color="gray">Original: {time} in timezone: {city}</span> */}
+			</div>
+		</div>
+	);
 }
 
 export default App;
